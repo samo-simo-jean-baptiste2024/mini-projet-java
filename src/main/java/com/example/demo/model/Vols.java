9eac2670cff227a670ce204b5ecb44591921c3a2
gracy.ai <gracesalatou@gmail.com>
2025-03-25 15:02:52 +0000

    modifs

--- src/main/java/com/example/demo/model/Vols.java
+++ src/main/java/com/example/demo/model/Vols.java
@@ -1,22 +1,32 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Setter
+@NoArgsConstructor
 @Getter
 @Entity
 public class Vols {
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    ,
     private String num_vol;
     private String ville_depart;
     private String ville_arrive;
     private Float frequence;
+
+    @ManyToMany
+    @JoinTable(
+            name = "vols_troncons",
+            joinColumns = @JoinColumn(name = "vols_id"),
+            inverseJoinColumns = @JoinColumn(name = "troncons_id")
+    )
+    private List<Troncons> troncons;
 }
