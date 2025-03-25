692f2b2fc4308f9d9fc29cd28a4997508a857fcf
gracy.ai <gracesalatou@gmail.com>
2025-03-25 15:12:59 +0000

    modifs

--- src/main/java/com/example/demo/model/Departs.java
+++ src/main/java/com/example/demo/model/Departs.java
@@ -1,9 +1,6 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +8,7 @@ import lombok.Setter;
 
 import java.sql.Time;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -24,4 +22,25 @@ public class Departs {
     private Long num_depart;
     private Date date;
     private Time heure_depart;
+
+    private  Float quantite_kerosene;
+
+    @ManyToMany
+    @JoinTable(
+            name = "departs_personnels",
+            joinColumns = @JoinColumn(name = "departs_id"),
+            inverseJoinColumns = @JoinColumn(name = "personnels_id")
+    )
+    private List<Personnels> personnels;
+    @ManyToMany
+    @JoinTable(
+            name = "departs_passagers",
+            joinColumns = @JoinColumn(name = "departs_id"),
+            inverseJoinColumns = @JoinColumn(name = "passagers_id")
+    )
+    private List<Passagers> passagers;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "vol_id")
+    private  Vols vols;
 }
